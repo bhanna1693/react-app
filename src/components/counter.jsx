a99af79b3f404976d1c420b56756276b62e21149
bhanna <bhanna@messerfinancial.com>
2019-07-01 15:59:28 -0400

    app 100%

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,29 +1,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0
-  };
-
-  constructor() {
-    super();
-    this.handleIncrement.bind(this);
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // Ajax call and get new date from server
+    }
   }
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
 
   render() {
+    console.log("Counter - Rendered");
+
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={ () => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -31,13 +37,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Stateless Functional Component (does not have lifecycle hooks)
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
